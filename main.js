900a535455f5005d7bfb4c1eeaa242e463a133b5
TZ-Design <yuxuanz6@andrew.cmu.edu>
2021-11-26 22:02:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,8 @@
         height = 400 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
-    var svg = d3.select("#datavis")
+    // select the div id you just created in html
+    var svg = d3.select("")
       .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -13,38 +14,37 @@
         .attr("transform",
               "translate(" + margin.left + "," + margin.top + ")");
 
-    // Parse the Data
+    // get and parse the Data
     d3.csv("https://raw.githubusercontent.com/TZ-Design/d3-tutorial/c08bede93475c70c0bfd4998c938d437fd560373/data.csv", function(data) {
 
-    // X axis
-    var x = d3.scaleBand()
-      .range([ 0, width ])
-      .domain(data.map(function(d) { return d.Country; }))
-      .padding(0.2);
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-      .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
-        .style("text-anchor", "end");
+    // // add x axis
+    // var x = d3.scaleBand()
+    //   .range([ 0, width ])
+    //   .domain(data.map(function(d) { return d.Country; }))
+    //   .padding(0.2);
+    // svg.append("g")
+    //   .attr("transform", "translate(0," + height + ")")
+    //   .call(d3.axisBottom(x))
+    //   .selectAll("text")
+    //     .attr("transform", "translate(-10,0)rotate(-45)")
+    //     .style("text-anchor", "end");
 
-    // Add Y axis
-    var y = d3.scaleLinear()
-      .domain([0, 13000])
-      .range([ height, 0]);
+    // // Add y axis
+    // var y = d3.scaleLinear()
+    //   .domain([0, 13000])
+    //   .range([ height, 0]);
     
-    svg.append("g")
-      .call(d3.axisLeft(y));
+    // svg.append("g")
+    //   .call(d3.axisLeft(y));
 
-    // Bars
+    // adding bars
     svg.selectAll("mybar")
       .data(data)
       .enter()
-      .append("rect")
+      .append("") //append rectangles (rect)
         .attr("x", function(d) { return x(d.Country); })
         .attr("y", function(d) { return y(d.Value); })
         .attr("width", x.bandwidth())
         .attr("height", function(d) { return height - y(d.Value); })
-        .attr("fill", "steelblue")
-
+        .attr("fill", "orange") //change the bar color to orange
     })
